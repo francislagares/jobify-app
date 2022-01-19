c481c0111165fab57d07d4dbb4697eea43251ab8
Francis Lagares <francis.lagares@gmail.com>
2022-01-19 21:35:39 +0100

    feat: create Error page content

--- client/src/views/Error/Error.tsx
+++ client/src/views/Error/Error.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '@/assets/images/not-found.svg';
+import Wrapper from '@/assets/wrappers/ErrorPage';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Ohh! Page not found</h3>
+        <p>We can't see the page you're looking for</p>
+        <Link to="/">Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
