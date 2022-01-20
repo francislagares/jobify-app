84254189562a7da311bc64cf1e4dc288f00669bc
Francis Lagares <francis.lagares@gmail.com>
2022-01-20 21:46:08 +0100

    test: move __mocks__ into src

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
   coveragePathIgnorePatterns: ['(test/.*.mock).(jsx?|tsx?)$'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
+      '<rootDir>/client/src/__mocks__/fileMock.ts',
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
   },
